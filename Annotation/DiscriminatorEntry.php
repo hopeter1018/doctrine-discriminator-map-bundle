667e6eeb3b757e360aeb394c78f2705f90b40d63
Peter <hokwaichi@gmail.com>
2020-07-14 10:06:58 +0800

    Better annotation

--- Annotation/DiscriminatorEntry.php
+++ Annotation/DiscriminatorEntry.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace HoPeter1018\DoctrineDiscriminatorMapBundle\Annotation;
 
+use Doctrine\Common\Annotations\Annotation;
+use Doctrine\Common\Annotations\Target;
+
 /**
  * @Annotation
+ * @Target({"CLASS"})
  */
 final class DiscriminatorEntry
 {
